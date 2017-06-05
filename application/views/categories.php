44e4f5917c40ca4dae73f5eb59293320ba223ccc
Karlygash Mukhitova <k.mukhitova@iitu.kz>
2017-06-05 14:30:58 +0600

    comment

--- application/views/categories.php
+++ application/views/categories.php
@@ -6,12 +6,13 @@ foreach ($blog_categories as $cats) {
     echo $cats['meta_keywords']."</br>";
     echo $cats['description']."</br>";
 }
+
 echo Form::open('categories.', array('method' => 'post'));
 echo Form::button('post', 'New Post', array('type' => 'submit'));
 echo Form::close();
 
 ?>
 
-
+// no comments
 
 
