36e64b650707bceaf47bb4691e719e28e5c8e76b
Karlygash Mukhitova <k.mukhitova@iitu.kz>
2017-06-15 09:54:13 +0600

    ig

---
+++ content/js/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by karlygashmukhitova on 6/14/17.
+ */
+localStorage.setItem('id', 'true');
+var param = window.location.search
+
+if (param==)
+
+.ajax({
+    type: "POST",
+    url: "posts/view_update",
+
+});
+
+/*decodeURIComponent(location.search.substr(1)).split('&')*/
